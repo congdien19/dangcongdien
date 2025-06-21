e5d6ca036e9798914e4000e772f259f308f488a4
Đặng Công Diện <141894358+congdien19@users.noreply.github.com>
2025-06-21 11:13:46 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -7,8 +8,15 @@ const Footer = () => {
       <div className="footer-left">
         © 2025. <a href="https://congdien.pro" target="_blank" rel="noopener noreferrer">congdien.pro</a> - Social Media Marketing.
       </div>
+      
       <div className="footer-right">
         Powered By <strong>Đặng Công Diện</strong>
+        <div className="footer-social">
+          <a href="https://facebook.com" className="facebook" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+          <a href="https://instagram.com" className="instagram" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="https://twitter.com" className="twitter" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+          <a href="https://linkedin.com" className="linkedin" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
+        </div>
       </div>
     </footer>
   );
